55c51175b5d177d891530b96ecdc311ddac2d4ae
birockaway <bi@rockaway.cz>
2018-08-15 13:57:00 +0200

    Update selen.py

--- src/selen.py
+++ src/selen.py
@@ -64,8 +64,8 @@ print("")
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--headless')
-driver = webdriver.Chrome('/usr/local/bin/chromedriver')
+#chrome_options.add_argument('--headless')
+driver = webdriver.Chrome('/usr/local/bin/chromedriver', chrome_options=chrome_options)
 
 driver.get("https://admin.zbozi.cz/loginScreen?url=%2F")
 
